03eb721cf603074bfdb273c72678ac380cc37bb4
Robin Hein Anna Matheus Friesen III <44802556+robijntje@users.noreply.github.com>
2020-10-12 12:10:43 +0200

    Created SpawnScript
    Created SpawnScript with two arrays for spawning balloons. Added to Unity hierarchy.

---
+++ AR Game 2/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{ 
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+}
